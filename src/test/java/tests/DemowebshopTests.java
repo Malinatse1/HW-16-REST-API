984f2bdb42340215a6d752144fab7e983b7d4fb7
bochk <natalya-maligon@mail.ru>
2023-02-18 13:09:08 +0300

    add tests

---
+++ src/test/java/tests/DemowebshopTests.java
@@ -0,0 +1,29 @@
+package tests;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class DemowebshopTests {
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "https://demowebshop.tricentis.com";
+    }
+
+    @Test
+    void changeCountryShippingToCartTest() {
+        given()
+                .cookie("Nop.customer","51f7aca9-65bf-49c5-a4f6-90cd364d2c6f")
+                .when()
+                .get("/country/getstatesbycountryid?countryId=87&addEmptyStateIfRequired=true&_=1676637499942")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id",is(0))
+                .body("name", is("Other (Non US)"));
+
+    }
+}
+
